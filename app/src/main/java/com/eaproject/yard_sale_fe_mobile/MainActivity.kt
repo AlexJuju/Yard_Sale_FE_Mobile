53b34fa35af262e3acd6ff3cc6f0e2d91c3c446e
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2023-10-18 13:31:00 +0200

    Dummy commit: Mutable & Immutable State Flow

--- app/src/main/java/com/eaproject/yard_sale_fe_mobile/MainActivity.kt
+++ app/src/main/java/com/eaproject/yard_sale_fe_mobile/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
 import com.eaproject.yard_sale_fe_mobile.ui.theme.Yard_Sale_FE_MobileTheme
+import kotlinx.coroutines.flow.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -75,13 +77,19 @@ fun TextFieldExample() {
 
 //@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting() {
+fun Greeting(user: UserViewModel) {
     var userFirstName by remember { mutableStateOf("") }
     var updated = 1
-    var obs by remember { mutableStateOf(" obs")}
+    var obs by remember { mutableStateOf(user.immutableUser)}
+
+    user._user.value.firstName = "pippo"
+    user.userVariable.value.firstName = "pippo"
+
+    user.immutableUser.firstName = "nuovo nome" //NON AGGIORNA L'OSSERVABILE (e quindi non lancia evento e ricomposizione)
+    obs.firstName = "nuovo nome" //NON AGGIORNA L'OGGETTO ORIGINALE (lancia evento e ricomposizione ma solo dell'osservabile)
     Column {
         Text(
-            text = "Hello ${updated} ${obs}!",
+            text = "Hello ${user._user.collectAsState().value.firstName}!",
         )
         TextField(
             value = userFirstName,
@@ -90,11 +98,41 @@ fun Greeting() {
         )
         Button(
             onClick = {
+                user.updateFirstName(userFirstName)
                 updated += 1
                 obs = userFirstName
                       },
         ) {
             Text(text = "Update")
         }
+    }
+}
+
+fun Pippo() {
+    var user : UserViewModel = "stesso oggetto di prima"
+
+    var obs2 by remember { mutableStateOf(user.immutableUser)}
+
+    obs2 //NON LANCERA' NESSUN EVENTO SE IMMUTABLEUSER O OBS VIENE AGGIORNATO DA UN'ALTRA PARTE
+}
+
+data class UserState (
+    var username: String = "",
+    var firstName: String = "",
+    var lastName: String = "",
+)
+
+class UserViewModel : ViewModel() {
+
+    val _user = MutableStateFlow(UserState(firstName = "Mario", lastName = "Rossi", username = "supermario"))
+    val userVariable: StateFlow<UserState> = _user.asStateFlow()
+
+    val immutableUser = UserState(firstName = "Mario", lastName = "Rossi", username = "supermario")
+
+    fun updateFirstName(firstName: String) {
+        _user.update { current -> current.copy(
+            username = current.username,
+            firstName = firstName,
+            lastName = current.lastName) }
     }
 }
