524b1172a1abf4a5178512e1b6458572c338f0c7
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2023-10-18 12:24:04 +0200

    Dummy commit

--- app/src/main/java/com/eaproject/yard_sale_fe_mobile/MainActivity.kt
+++ app/src/main/java/com/eaproject/yard_sale_fe_mobile/MainActivity.kt
@@ -3,11 +3,10 @@ package com.eaproject.yard_sale_fe_mobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.eaproject.yard_sale_fe_mobile.ui.theme.Yard_Sale_FE_MobileTheme
@@ -22,22 +21,80 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
+//                    var clickCount by remember { mutableStateOf(0)}
+//                    ClickCounter(clicks = clickCount, onClick = {
+//                        clickCount += 1
+//                    })
+
                 }
             }
         }
     }
 }
 
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    Yard_Sale_FE_MobileTheme {
+//        Greeting("Android")
+//    }
+//}
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ClickCounter(clicks: Int, onClick: () -> Unit) {
+    Column() {
+        Button(onClick = onClick) {
+            Text(text = "Number of clicks: $clicks")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
+//@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DefaultPreview() {
-    Yard_Sale_FE_MobileTheme {
-        Greeting("Android")
+fun TextFieldExample() {
+//    var myContent by remember { mutableStateOf("") }
+     var (myContent, setValue) = remember { mutableStateOf("") }
+//    setValue("ciao")
+    Column {
+        TextField(
+            value = myContent,
+            onValueChange = { content -> myContent = content },
+            // onValueChange = { setValue(it) },
+            label = { Text("") },
+            placeholder = { Text(text = "Your content") }
+        )
+    }
+}
+
+//@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Greeting() {
+    var userFirstName by remember { mutableStateOf("") }
+    var updated = 1
+    var obs by remember { mutableStateOf(" obs")}
+    Column {
+        Text(
+            text = "Hello ${updated} ${obs}!",
+        )
+        TextField(
+            value = userFirstName,
+            onValueChange = { content -> userFirstName = content },
+            placeholder = { Text(text = "First Name") }
+        )
+        Button(
+            onClick = {
+                updated += 1
+                obs = userFirstName
+                      },
+        ) {
+            Text(text = "Update")
+        }
     }
 }
