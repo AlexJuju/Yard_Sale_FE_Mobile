1d6b9e823517d789d67b640fcbfd353cc49e9b20
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-10-23 18:37:56 +0200

    Test modifiers sui vari componenti.

--- app/src/main/java/com/eaproject/yard_sale_fe_mobile/MainActivity.kt
+++ app/src/main/java/com/eaproject/yard_sale_fe_mobile/MainActivity.kt
@@ -5,16 +5,20 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import com.eaproject.yard_sale_fe_mobile.ui.theme.Yard_Sale_FE_MobileTheme
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -24,62 +28,66 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            Yard_Sale_FE_MobileTheme {
+            Yard_Sale_FE_MobileTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
                     var state by remember { mutableStateOf(false) }
-                    if(state)
+                    if (state) {
                         compOne { state = false }
-                    else
+                    } else {
                         compTwo { state = true }
-
-//                    val viewModel = username()
-//                    loginPage(viewModel)
-//                    homePage(viewModel)
-//                    this.application
-
-//                    Greeting()
+                    }
                 }
-//            }
+            }
         }
     }
 }
 
 @Composable
 fun compOne(onContentChange: () -> Unit){
-    LazyColumn{item {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LazyColumn {
+            item {
+                    Button(
+                        onClick = onContentChange,
+                        modifier = Modifier
+                            .background(Color.Yellow)
+                            .fillMaxSize()
+                            .padding(50.dp)
 
-        Button(
-            onClick = onContentChange
-        ) {
-            val modifier = Modifier
-            Text("vai a pagina 2",
-                modifier
-                    .padding(50.dp)
-//                    .background(Color.Black)
-                    .weight(2f)
-                    ,
-                color = Color.Red,
-//                style = TextStyle(color = Color.Red)
-            )
-        }
-    }}
+                    ) {
+                        Text(
+                            "vai a pagina 1",
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                color = Color.Red,
+                                textAlign = TextAlign.Center,
+                                background = Color.Black
+                            )
+                        )
+                    }
+                }
+            }
+    }
 }
-
 @Composable
 fun compTwo( onContentChange: () -> Unit ){
     LazyColumn{item {
         Button(
             onClick = onContentChange
         ) {
-            Text("vai a pagina 1")
+            Text("vai a pagina 2")
         }
     }}
 }
-
+/*********************************************************************************/
 class username : ViewModel() {
     val userInput = MutableStateFlow("")
     val user: StateFlow<String> = userInput
