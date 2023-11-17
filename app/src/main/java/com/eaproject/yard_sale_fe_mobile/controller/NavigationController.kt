a8258405e95832ffe840bea7f05d4345de3e6381
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-11-17 21:05:43 +0100

    Test.

--- app/src/main/java/com/eaproject/yard_sale_fe_mobile/controller/NavigationController.kt
+++ app/src/main/java/com/eaproject/yard_sale_fe_mobile/controller/NavigationController.kt
@@ -6,24 +6,24 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.eaproject.yard_sale_fe_mobile.controller.utils.ScreenNav
 import com.eaproject.yard_sale_fe_mobile.core.Username
-import com.eaproject.yard_sale_fe_mobile.layout.compOne
-import com.eaproject.yard_sale_fe_mobile.layout.compTwo
+import com.eaproject.yard_sale_fe_mobile.layout.loginPage
+import com.eaproject.yard_sale_fe_mobile.layout.homePage
 
 @Composable
 fun navigationTest(navController: NavHostController, viewModel: Username) {
-    NavHost(
-        navController = navController,
-        startDestination = ScreenNav.PageOne.name
-    ) {
-        composable(ScreenNav.PageOne.name) {
-            compOne(viewModel) {
-                navController.navigate(ScreenNav.PageTwo.name)
-            }
-        }
-        composable(ScreenNav.PageTwo.name) {
-            compTwo(viewModel) {
-                navController.navigate(ScreenNav.PageOne.name)
-            }
-        }
-    }
+//    NavHost(
+//        navController = navController,
+//        startDestination = ScreenNav.Login.name
+//    ) {
+//        composable(ScreenNav.Login.name) {
+//            loginPage(viewModel) {
+//                navController.navigate(ScreenNav.Homepage.name)
+//            }
+//        }
+//        composable(ScreenNav.Homepage.name) {
+//            homePage(viewModel) {
+//                navController.navigate(ScreenNav.Login.name)
+//            }
+//        }
+//    }
 }
--- app/src/main/java/com/eaproject/yard_sale_fe_mobile/core/Username.kt
+++ app/src/main/java/com/eaproject/yard_sale_fe_mobile/core/Username.kt
@@ -1,10 +1,16 @@
 package com.eaproject.yard_sale_fe_mobile.core
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import java.io.Serializable
 
-class Username : ViewModel() {
+//https://stackoverflow.com/questions/47593205/how-to-pass-custom-object-via-intent-in-kotlin
+//https://developer.android.com/reference/kotlin/android/content/Intent
+class Username() : ViewModel(){
+    /*------------------------------------------------------------*/
     private val userInput = MutableStateFlow("")
     val user: StateFlow<String> = userInput
 
